7473bc273d323382edf6889586e15c2ff8b81280
pransju09 <82951689+pransju09@users.noreply.github.com>
2021-10-06 17:50:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+noseX = 0;
+noseY = 0;
+
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("PoseNet is Initialized");
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(clown_nose , noseX  -15, noseY -15, 30 , 30);
+}
+
+function take_snapshot() {
+    save('myFiterImage.png');
+}
+
+function gotPoses(result) {
+    if (result.length > 0) {
+        console.log(result);
+        noseX = result[0].pose.nose.x;
+        noseY = result[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
+
